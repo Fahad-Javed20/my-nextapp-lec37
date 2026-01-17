1ebcc693e317bfd3694b973909e06f0242fdc8df
Fahad Bin Javed <126807388+Fahad-Javed20@users.noreply.github.com>
2026-01-17 12:49:26 +0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div >
      <h1>This is the HomePage.</h1>
+     <Link className="font-bold text-red-600" href="/about">About</Link>
     </div>
   );
 }
